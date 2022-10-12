4832dc7051847ab270d7bb6c1d877bda1a93f295
Guilherme <Guilherme@DESKTOP-JTEAKPM>
2022-10-12 04:24:44 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using ConvertPDF;
+using InteropWindows2;
+using PDFsharpCore;
+
+
+
+ConvertWordToPdf();
+ConvertImgToPdf();
+
+Console.ReadLine();
+
+
+
+
+
+static void ConvertImgToPdf()
+{
+    PdfHelper.SaveImageAsPdf("C:\\Users\\Guilherme\\Desktop\\Teste01.jpg", "C:\\Users\\Guilherme\\Desktop\\Teste01.pdf");
+
+    Console.WriteLine("Imagem convertida para PDF.");
+}
+
+static void ConvertWordToPdf()
+{
+    var pathPdf = ConvertInterop.WordToPdf("C:\\Users\\Guilherme\\Desktop\\Doc01.docx", "C:\\Users\\Guilherme\\Desktop\\Doc01.pdf");
+
+    Console.WriteLine("Documento Word convertido para PDF.");
+
+    //Console.WriteLine("Aperte ENTER para remover o documento");
+    //Console.ReadLine();
+
+    //if (File.Exists(pathPdf)) File.Delete(pathPdf);
+
+    //Console.WriteLine("Documento removido.");
+    //Console.ReadLine();
+}
+
+static void Test(string pathPdf)
+{
+    string fileString;
+
+    using (var filePdf = new StreamReader(pathPdf))
+    {
+        fileString = Utils.ConvertToBase64(filePdf.BaseStream);
+    }
+}
+
+
+
+
+
---
+++ Utils.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace ConvertPDF
+{
+    public class Utils
+    {
+        public static string ConvertToBase64(Stream stream)
+        {
+            byte[] bytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                stream.CopyTo(memoryStream);
+                bytes = memoryStream.ToArray();
+            }
+
+            return Convert.ToBase64String(bytes);
+            //return new MemoryStream(Encoding.UTF8.GetBytes(base64));
+        }
+    }
+}
